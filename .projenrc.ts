04b4adc8a6300221f878c8afd04f2b95263de518
Lorcan O'Flynn <lorcan.oflynn@gmail.com>
2025-06-03 14:24:19 +0100

    initial commit

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,20 @@
 import { typescript } from 'projen';
+
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'firstblox-aws-tools-ts',
   projenrcTs: true,
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  devDeps: ["tsx"],
+  deps: [
+    '@aws-sdk/client-iam',
+    "@aws-sdk/client-s3",
+    '@aws-sdk/credential-provider-env',
+    'commander',
+  ],
 });
+
+project.addScripts({
+  "list-buckets": "tsx ./src/lib/scripts/list-buckets.ts",
+});
+
 project.synth();
